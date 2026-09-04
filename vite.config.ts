3307e3cea48f04902fcae162b0088b91b2b2a0e4
Marco Pagan <53830761+marcopagan@users.noreply.github.com>
2026-09-04 19:26:35 +0200

    Update adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -11,8 +10,7 @@ export default defineConfig({
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter()
+			}
 		})
 	]
 });
