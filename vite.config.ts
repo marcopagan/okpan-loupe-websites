910de53ec3b7f2a24f8cb30dbb121726555f90d6
Marco Pagan <53830761+marcopagan@users.noreply.github.com>
2026-09-07 12:12:42 +0200

    Adding static adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-static';
 
 export default defineConfig({
 	plugins: [
@@ -10,7 +11,13 @@ export default defineConfig({
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
+			},
+			adapter: adapter({
+        		fallback: '404.html'
+      		}),
+			paths: {
+        		base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH ?? '')
+      		}
 		})
 	]
 });
